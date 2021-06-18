80564bef8cc010cce40dc868f530280a9ad4afe0
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-06-18 11:03:55 +0100

    updates

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 import seaborn as sns
 sns.set()
 from sklearn.cluster import KMeans
-
-data = 
+from sklearn import preprocessing
 
