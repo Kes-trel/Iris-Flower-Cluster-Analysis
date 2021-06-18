6a9b36128b334ad8ea31c8b93bf2f90d4e8eb3b1
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-06-18 15:40:43 +0100

    ready to delpoy

--- functions.py
+++ functions.py
@@ -2,53 +2,57 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 
-def scale_data(data):
-    scaler = preprocessing.StandardScaler()
-    scaler.fit(data)
-    data_scaled = scaler.transform(data)
-    return data_scaled
-
 def screen_data(petal_sepal, data):
     data_select = data.copy()
-    if petal_sepal == "Sepal":
+    if petal_sepal == "sepal":
         data_select = data.iloc[:, :-3]
         return data_select
-    elif petal_sepal == "Petal":
+    elif petal_sepal == "petal":
         data_select = data.iloc[:, 2:-1]
         return data_select
 
-def scater_plot(data, value):
+def scater_plot(data, value, size):
     x_values = f"{value}_length"
     y_values = f"{value}_width"
     x_label = f"Length of {value}"
     y_label = f"Width of {value}"
-
+    fig, ax = plt.subplots(figsize=size)
     if "clusters" in data.columns:
-        plt.scatter(data[x_values], data[y_values], c=data["clusters"], cmap="rainbow")
+        ax.scatter(data[x_values], data[y_values], c=data["clusters"], cmap="rainbow")
     else:
-        plt.scatter(data[x_values], data[y_values])
-        
+        ax.scatter(data[x_values], data[y_values])
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    return plt.show()
+    return fig
 
-def elbow_method(clusters, data):
+def elbow_method(clusters, data, size):
     wcss = list()
     for c in range(1, clusters+1):
         kmeans = KMeans(c)
         kmeans.fit(data)
         wcss_iter = kmeans.inertia_
         wcss.append(wcss_iter)
-    
-    plt.plot(range(1, clusters+1), wcss)
+    fig, ax = plt.subplots(figsize=size)
+    ax.plot(range(1, clusters+1), wcss)
     plt.title('The Elbow Method')
     plt.xlabel('Number of clusters')
     plt.ylabel('Within-cluster Sum of Squares')
-    return plt.show()
+    return fig
 
 def cluster_data(number_of_clusters, data):
     data_c = data.copy()
     kmeans = KMeans(number_of_clusters)
     kmeans.fit(data_c)
     data_c["clusters"] = kmeans.fit_predict(data_c)
+    return data_c
+
+def scale_cluster_data(number_of_clusters, data):
+    scaler = preprocessing.StandardScaler()
+    scaler.fit(data)
+    data_scaled = scaler.transform(data)
+
+    data_c = data.copy()
+    kmeans = KMeans(number_of_clusters)
+    kmeans.fit(data_scaled)
+    data_c["clusters"] = kmeans.fit_predict(data_scaled)
     return data_c
--- main.py
+++ main.py
@@ -7,37 +7,71 @@ sns.set()
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 
-from functions import scale_data, screen_data, scater_plot, elbow_method, cluster_data
+from functions import screen_data, scater_plot, elbow_method, cluster_data, scale_cluster_data
 
 raw_data = pd.read_csv("iris_with_species.csv")
 
 st.set_page_config(page_title="Species Segmentation with Cluster Analysis",page_icon="🌼", layout="wide", initial_sidebar_state="auto")
 
-petal_or_sepal = st.sidebar.radio("Select data for analysis", ["Sepal", "Petal"])
+col_a, col_b = st.beta_columns([4,1])
+col_a.title("Iris flower data set")
+col_a.header("Species Segmentation with Cluster Analysis (KMeans)")
+col_b.image("https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1200px-Scikit_learn_logo_small.svg.png")
+
+show_real_data = st.sidebar.checkbox("Show real data", help="Test some clusters yourself first")
+
+width = st.sidebar.slider("Plot width", min_value=1, max_value=25, step=1, value=12, format="%i")
+height = st.sidebar.slider("Plot height", min_value=1, max_value=25, step=1, value=5, format="%i")
+plot_size = (width, height)
+
+petals_or_sepals = st.sidebar.radio("Select data for analysis", ["sepal", "petal"])
+
 scale_data_box = st.sidebar.checkbox("Scale Data", help="Standardize features by removing the mean and scaling to unit variance. For more info read here: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html#")
-
-
-data = screen_data(petal_sepal=petal_or_sepal, data=raw_data)
-
-st.write(data)
-
-#inputs from streamlit app
-
-# BUFFER_SIZE = st.sidebar.number_input("Buffer Size", value=100, min_value=1, step=100, format="%i", help="")
-# BATCH_SIZE = st.sidebar.number_input("Batch Size", value=100, min_value=1, step=100, format="%i", help="")
-# hidden_layers_number = st.sidebar.number_input("Number of Hidden Layers", value=2, min_value=1, step=1, format="%i", help="")
-# hidden_layer_size = st.sidebar.number_input("Hidden Layer Unit Size", value=50, min_value=1, step=10, format="%i", help="")
-# activation_function = st.sidebar.selectbox("Select Activation Function", activation_functions_list, index=7, help="")
-# model_optimizer = st.sidebar.selectbox("Select Model Optimiser Class", model_optimizer_list, index=2, help="")
-# model_loss = st.sidebar.selectbox("Select Model Loss", model_loss_list, index=28, help="")
-# NUM_EPOCHS = st.sidebar.number_input("Number of epochs", value=5, min_value=1, step=1, format="%i", help="")
 
 test_clusters = st.sidebar.number_input("Test clusters", value=1, min_value=1, step=1, format="%i")
 
 
-if st.sidebar.checkbox("Get help with Elbow Method"):
+data_screen = screen_data(data=raw_data, petal_sepal=petals_or_sepals)
+
+if scale_data_box:
+    data = scale_cluster_data(data=data_screen, number_of_clusters=test_clusters)
+else:
+    data = cluster_data(data=data_screen, number_of_clusters=test_clusters)
+
+real_results = raw_data.copy()
+real_results["clusters"] = real_results["species"].map({"setosa":0, "versicolor":1 , "virginica":2})
+
+if show_real_data:
+    col1, col2 = st.beta_columns(2)
+    col1.subheader("Your analysis")
+    col1.pyplot(scater_plot(data=data, value=petals_or_sepals, size=plot_size))
+    
+    col2.subheader("Real data")
+    col2.pyplot(scater_plot(data=real_results, value=petals_or_sepals, size=plot_size))
+else:
+    st.pyplot(scater_plot(data=data, value=petals_or_sepals, size=plot_size))
+
+if st.sidebar.checkbox("Get a hint with Elbow Method"):
     number_of_cluster_elbow = st.sidebar.number_input("Select number of cluster", value=5, min_value=1, step=1, format="%i", help="""Cluster number keeps track the highest number of clusters we want to use the WCSS method for.
     More info https://en.wikipedia.org/wiki/Elbow_method_(clustering)""")
 
-    st.pyplot(elbow_method(clusters=number_of_cluster_elbow, data=data))
+    st.pyplot(elbow_method(data=data, clusters=number_of_cluster_elbow, size=plot_size))
 
+conclusion = """
+    The original dataset has 3 sub-species of the Iris flower. Therefore, the number of clusters is 3.
+    Read more here: https://en.wikipedia.org/wiki/Iris_flower_data_set
+    
+    This shows us that:
+
+    * the Eblow method is imperfect (we might have opted for 2 or even 4 clusters)
+    * k-means is very useful in moments where we already know the number of clusters - in this case: 3
+    * biology cannot be always quantified   
+    """
+
+iris = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Iris_versicolor_3.jpg/1280px-Iris_versicolor_3.jpg"
+
+if show_real_data:
+    with st.beta_expander("Iris flower data set"):
+        col_1, col_2 = st.beta_columns([2,1])
+        col_1.write(conclusion)
+        col_2.image(iris)
